6748e772f4dadedc15ffbd252127fa79df425567
luisgcodes <lcienfuegos95@gmail.com>
2022-04-03 20:54:22 -0700

    linking weather apikey

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
-let APIKey = "58c181ba70d6d89d42febadc51ef7d70";
-let locations = [];
+let apiKey = "58c181ba70d6d89d42febadc51ef7d70";
+let searchedLocations = [];
 
 function getlocalData(lat, lon, city) {
 
-    var queryURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=,minutely,hourly,alerts&appid=" + APIKey;
+    var queryURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=,minutely,hourly,alerts&appid=" + apiKey;
 
     $.ajax({
         url: queryURL,
@@ -16,3 +16,27 @@ function getlocalData(lat, lon, city) {
 
         });           
  };
+
+ function loadWeatherlocation(city, isClicked) {
+
+    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?zip=" + zipCpde + ",us&appid=" + apiKey;
+    var weatherContent = $("#weatherContent");
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+        .then(function (response) { 
+
+            if (!isClicked)
+            {
+                saveLocations(response);  
+                renderLocations();
+            }
+
+            getlocalData(response.city.coord.lat, response.city.coord.lon, response.city.name);
+
+        }).catch(function (response){
+            alert("Location Not Valid")
+        });
+}
